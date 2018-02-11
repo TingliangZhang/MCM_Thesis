cd5c257dfc08d7bee927cac602c03c365ceb2802
TingliangZhang <32335780+TingliangZhang@users.noreply.github.com>
2018-02-11 19:43:47 +0800

    FFF

--- code/Voronoi.m
+++ code/Voronoi.m
@@ -1,6 +1,17 @@
 X = xlsread('LocationUSA.xlsx','A1:A549');
 Y = xlsread('LocationUSA.xlsx','B1:B549');
+Z = xlsread('USA.xlsx','G2:G550');
+hold on;
 
 
 voronoi(X,Y);
+
+xlim([min(X) max(X)])
+ylim([min(Y) max(Y)])
+
+[vx,vy] = voronoi(X,Y);
+[v,c] = voronoin([X(:) Y(:)]);
+%plot(vx,vy,'.','markersize',9);
+%scatplot(vx,vy)
+hold off;
 
